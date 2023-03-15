a341dd55395dd0d0d2a6ee1d16c1490bee0efa8b
Josh Fenton <jfenton@highlib.local>
2023-03-15 17:00:15 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function randomAlerts() {
-    var alerts = new Array (
-    "Nice one!",
-    "I bet it's fancy!",
-    "Whazzit, whosa! Wowza!",
-    "That is a neat ride!",
-    "Live long and prosper.");
+    var msg = new Array (
+    "Whoa! -- Keanu Reeves",
+    "Believe you can and you're halfway there. -- Teddy Roosevelt",
+    "I'm not here to be perfect. I'm here to be real. -- Lady Gaga",
+    "We may encounter many defeats but we must not be defeated. -- Maya Angelou",
+    "If you want something done, ask a busy person to do it. -- Laura Ingalls Wilder",
+    "The best way to find out if you can trust somebody is to trust them. -- Ernest Hemingway",
+    "You're not getting a raise. -- My Boss",
+    "True terror is to wake up one morning and discover that your high school class is running the country. -- Kurt Vonnegut",
+    "Always forgive your enemies; nothing annoys them so much. -- Oscar Wilde",
+    "Never go to bed mad. Stay up and fight. -- Phyllis Diller");
     
-    var i = Math.floor(Math.random()*alerts.length);
-    
-    
-   window.alert(alerts[i]);
+    let i = Math.floor(Math.random()*msg.length);
+    window.alert(msg[i]);
 }
